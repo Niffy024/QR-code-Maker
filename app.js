c527561ba940915b6155dc8ded4d7d6aabb46d73
Macintosh HD <macintoshhd@Macintoshs-MacBook-Air.local>
2026-03-17 04:31:38 -0700

    feat: finalize QR Code Maker app
    - Implement QR code generation using qrcode library
    - Add responsive EJS frontend styling
    - Handle POST form submission with Express
    - Display generated QR dynamically
    - Add QR code download functionality
    - Configure static assets and project structure

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ejs from "ejs";
+import QRCode from 'qrcode';
 
 
 const app=express();
@@ -8,13 +9,25 @@ const port=3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+async function QRCodeGenerator(text) {
+  try {
+    const result = await QRCode.toDataURL(text);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 app.get("/",(req,res)=>{
-res.render("index.ejs");
+res.render("index.ejs",{qr:null});
 });
 
-app.post("/generate",(req,res)=>{
+app.post("/generate", async (req,res)=>{
     var url= req.body.url;
-    console.log(url);
+    //console.log(url);
+    const result= await QRCodeGenerator(url);
+    res.render("index.ejs", {qr: result});
+
 });
 
 app.listen(port,()=>{
